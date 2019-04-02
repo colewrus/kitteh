99ae43091e33295dbb4f15a906d257d5493cb4a2
colewrus <wrampelc@gmail.com>
2019-04-02 07:30:48 -0400

    version updated. Models imported

---
+++ Assets/catScript0.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catScript0 : MonoBehaviour
+{
+
+
+    float H;
+    float V;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        H = Input.GetAxis("Horizontal");
+        V = Input.GetAxis("Vertical");
+        Debug.Log(H + ", " + V);
+    }
+}
